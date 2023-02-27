483d4f30525c233830c358e6aa5178317ebda22b
Albert Cardona II <bigdaddycardona@live.com>
2023-02-26 21:23:39 -0500

    added some javascript and some partial scss back. I am redoing the design :) I am trying to work on the functionality but without some style I just can't. I redid my tables and navbar. everything is good now Next is footer and cards again. I might just put the cards back to the same with different colors

--- MUC.Web/wwwroot/js/site.js
+++ MUC.Web/wwwroot/js/site.js
@@ -1,1 +1,15 @@
-﻿
+﻿document.addEventListener("click", e => {
+   
+    const dropdownLinks = document.querySelector("[data-dropdown-links]");   
+
+    if (e.target.matches("[data-dropdown-button]")) {
+        dropdownLinks.classList.toggle("is-visible");
+        console.log("did it work")
+    }
+
+    if (!e.target.matches("[data-dropdown-button]") && dropdownLinks.classList.contains("is-visible")) {
+        dropdownLinks.classList.remove("is-visible");
+    }
+   
+})
+

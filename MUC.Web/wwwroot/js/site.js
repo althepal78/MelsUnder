2ee761d87a7a80a8ae284128c4740272aed3798b
Albert Cardona II <bigdaddycardona@live.com>
2023-03-02 11:21:03 -0500

    now that I got things looking better I am going to work on the menu parts first the look lol

--- MUC.Web/wwwroot/js/site.js
+++ MUC.Web/wwwroot/js/site.js
@@ -1,15 +1,37 @@
-﻿document.addEventListener("click", e => {
-   
-    const dropdownLinks = document.querySelector("[data-dropdown-links]");   
+﻿
 
-    if (e.target.matches("[data-dropdown-button]")) {
-        dropdownLinks.classList.toggle("is-visible");
-        console.log("did it work")
-    }
+document.addEventListener("click", e => {
+    // for the dropdown menu on the navbar
 
-    if (!e.target.matches("[data-dropdown-button]") && dropdownLinks.classList.contains("is-visible")) {
-        dropdownLinks.classList.remove("is-visible");
+    try {
+        const dropdownLinks = document.querySelector("[data-dropdown-links]");
+
+        if (e.target.matches("[data-dropdown-button]")) {
+            dropdownLinks.classList.toggle("is-visible");
+            console.log("did it work")
+        }
+
+        if (!e.target.matches("[data-dropdown-button]") && dropdownLinks.classList.contains("is-visible")) {
+            dropdownLinks.classList.remove("is-visible");
+        }
+    }catch (error) {
+        console.log(error + ":  **** is the freaking error")
     }
-   
 })
 
+
+  // this is for the cards section
+try {
+    document.querySelector("#cards").onmousemove = e => {
+        for (const card of document.querySelectorAll(".card")) {
+            const rect = card.getBoundingClientRect(),
+                x = e.clientX - rect.left,
+                y = e.clientY - rect.top;
+
+            card.style.setProperty("--mouse-x", `${x}px`);
+            card.style.setProperty("--mouse-y", `${y}px`);
+        }
+    }
+} catch (err) {
+    console.log("Card beauty Error: " + err);
+}
